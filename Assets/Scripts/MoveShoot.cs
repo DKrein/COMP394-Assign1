0a0f1117437c2d4df407b84fd7a6f9cc2742db7c
DKrein <douglaskrein@gmail.com>
2015-09-22 20:07:42 -0400

    Add player controller and background

---
+++ Assets/Scripts/MoveShoot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShoot : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		//GetComponent<Rigidbody2D>().velocity = transform * speed;
+		GetComponent<Rigidbody2D> ().AddForce (transform.up * speed * 2);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float fireRate;
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	private Vector3 rotatePlayer;
+	private float nextFire;
+
+	void Update ()
+	{
+		if (Input.GetKey("space") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate () {
+		float rotatePlayer = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		transform.Rotate(new Vector3 (0, 0,rotatePlayer * speed * 2 * -1));
+		transform.Translate (new Vector3 (moveVertical * speed, 0, 0) * Time.deltaTime);
+
+		if (transform.position.y > 5)
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 10, transform.position.z);
+		else if (transform.position.y < -5)
+			transform.position = new Vector3 (transform.position.x, transform.position.y + 10, transform.position.z);
+		else if (transform.position.x > 5.3f)
+			transform.position = new Vector3 (transform.position.x - 10.6f, transform.position.y, transform.position.z);
+		else if (transform.position.x < -5.3f)
+			transform.position = new Vector3 (transform.position.x + 10.6f, transform.position.y, transform.position.z);
+
+	}
+}
